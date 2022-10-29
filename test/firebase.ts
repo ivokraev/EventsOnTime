5e0bcaf3b22ebf77816e26d6b9d8a60da782a01c
ivo <ivo.kraev2@protonmail.com>
2022-10-29 10:31:29 +0300

    Add firebase

--- test/firebase.ts
+++ test/firebase.ts
@@ -1,8 +1,18 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  //...
+    apiKey: "AIzaSyCh6brjG63-oe1CmqY9uf3hfOizmeIGW0g",
+    authDomain: "events-on-time.firebaseapp.com",
+    projectId: "events-on-time",
+    storageBucket: "events-on-time.appspot.com",
+    messagingSenderId: "478256429739",
+    appId: "1:478256429739:web:7c46d3652e59a9f2d655ee",
+    measurementId: "G-3P2CD8BK32"
 };
+  
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
