86c1c14a04d350ec4babfabca658fb5a134e5d5e
ivo <ivo.kraev2@protonmail.com>
2022-10-29 09:50:32 +0300

    Add typescript

---
+++ test/firebase.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var app_1 = require("firebase/app");
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+//...
+};
+var app = (0, app_1.initializeApp)(firebaseConfig);
