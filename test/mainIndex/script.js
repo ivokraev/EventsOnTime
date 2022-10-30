e80031a8da1445250e931d2c0fbb0896e6c27d63
User <toli100ilov@gmail.com>
2022-10-30 10:47:36 +0200

    final

--- test/mainIndex/script.js
+++ test/mainIndex/script.js
@@ -1,121 +1,121 @@
 // ---------Responsive-navbar-active-animation-----------
 function test() {
-    var tabsNewAnim = $("#navbarSupportedContent");
-    var selectorNewAnim = $("#navbarSupportedContent").find("li").length;
-    var activeItemNewAnim = tabsNewAnim.find(".active");
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
+  var tabsNewAnim = $("#navbarSupportedContent");
+  var selectorNewAnim = $("#navbarSupportedContent").find("li").length;
+  var activeItemNewAnim = tabsNewAnim.find(".active");
+  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  var itemPosNewAnimTop = activeItemNewAnim.position();
+  var itemPosNewAnimLeft = activeItemNewAnim.position();
+  $(".hori-selector").css({
+    top: itemPosNewAnimTop.top + "px",
+    left: itemPosNewAnimLeft.left + "px",
+    height: activeWidthNewAnimHeight + "px",
+    width: activeWidthNewAnimWidth + "px",
+  });
+  $("#navbarSupportedContent").on("click", "li", function (e) {
+    $("#navbarSupportedContent ul li").removeClass("active");
+    $(this).addClass("active");
+    var activeWidthNewAnimHeight = $(this).innerHeight();
+    var activeWidthNewAnimWidth = $(this).innerWidth();
+    var itemPosNewAnimTop = $(this).position();
+    var itemPosNewAnimLeft = $(this).position();
     $(".hori-selector").css({
       top: itemPosNewAnimTop.top + "px",
       left: itemPosNewAnimLeft.left + "px",
       height: activeWidthNewAnimHeight + "px",
-      width: activeWidthNewAnimWidth + "px"
+      width: activeWidthNewAnimWidth + "px",
     });
-    $("#navbarSupportedContent").on("click", "li", function (e) {
-      $("#navbarSupportedContent ul li").removeClass("active");
-      $(this).addClass("active");
-      var activeWidthNewAnimHeight = $(this).innerHeight();
-      var activeWidthNewAnimWidth = $(this).innerWidth();
-      var itemPosNewAnimTop = $(this).position();
-      var itemPosNewAnimLeft = $(this).position();
-      $(".hori-selector").css({
-        top: itemPosNewAnimTop.top + "px",
-        left: itemPosNewAnimLeft.left + "px",
-        height: activeWidthNewAnimHeight + "px",
-        width: activeWidthNewAnimWidth + "px"
-      });
-    });
+  });
+}
+$(document).ready(function () {
+  setTimeout(function () {
+    test();
+  });
+});
+$(window).on("resize", function () {
+  setTimeout(function () {
+    test();
+  }, 500);
+});
+$(".navbar-toggler").click(function () {
+  $(".navbar-collapse").slideToggle(300);
+  setTimeout(function () {
+    test();
+  });
+});
+
+// --------------add active class-on another-page move----------
+jQuery(document).ready(function ($) {
+  // Get current path and find target link
+  var path = window.location.pathname.split("/").pop();
+
+  // Account for home page with empty path
+  if (path == "") {
+    path = "index.html";
   }
-  $(document).ready(function () {
-    setTimeout(function () {
-      test();
-    });
+
+  var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
+  // Add active class to target link
+  target.parent().addClass("active");
+});
+
+const card = [
+  '<a href="../events/',
+  '.html" class="card"><div class="imgBx"><img src="',
+  '" alt="image_profile"></div><div class="content"><div class="details"><h2>',
+  '</h2><div class="data"><h3>',
+  "<br><span>",
+  "</span></h3><h3>",
+  "<br> <span>",
+  "</span></h3><h3>",
+  "<br> <span>",
+  "</span></h3></div></div></div></a>",
+];
+const cardBox = document.querySelector(".cardBox");
+let events;
+
+fetch("../../events.json")
+  .then((response) => response.json())
+  .then((json) => {
+    events = json.events;
+    printEvents("all");
   });
-  $(window).on("resize", function () {
-    setTimeout(function () {
-      test();
-    }, 500);
-  });
-  $(".navbar-toggler").click(function () {
-    $(".navbar-collapse").slideToggle(300);
-    setTimeout(function () {
-      test();
-    });
-  });
-  
-  // --------------add active class-on another-page move----------
-  jQuery(document).ready(function ($) {
-    // Get current path and find target link
-    var path = window.location.pathname.split("/").pop();
-  
-    // Account for home page with empty path
-    if (path == "") {
-      path = "index.html";
+
+function printEvents(tag) {
+  events.forEach((element) => {
+    console.log(element.seasonTag);
+    if ((tag == "all" || element.seasonTag == tag)) {
+      let currentCard = "".concat(card[0]);
+      currentCard = currentCard.concat(element.token);
+      currentCard = currentCard.concat(card[1]);
+
+      currentCard = currentCard.concat(element.imgURL);
+      currentCard = currentCard.concat(card[2]);
+      currentCard = currentCard.concat(element.eventName);
+
+      currentCard = currentCard.concat(card[3]);
+      currentCard = currentCard.concat(card[4]);
+      currentCard = currentCard.concat(card[5]);
+      currentCard = currentCard.concat(card[6]);
+      currentCard = currentCard.concat(card[7]);
+      currentCard = currentCard.concat(card[8]);
+      currentCard = currentCard.concat(card[9]);
+      cardBox.innerHTML += currentCard;
     }
-  
-    var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
-    // Add active class to target link
-    target.parent().addClass("active");
   });
-  
+}
 
-  const card = ["<a href=\"../events/", ".html\" class=\"card\"><div class=\"imgBx\"><img src=\"", "\" alt=\"image_profile\"></div><div class=\"content\"><div class=\"details\"><h2>", "</h2><div class=\"data\"><h3>", "<br><span>", "</span></h3><h3>", "<br> <span>", "</span></h3><h3>", "<br> <span>", "</span></h3></div></div></div></a>"];
-  const cardBox = document.querySelector(".cardBox");
-  let events;
+const buttons = document.querySelectorAll("button");
+buttons.forEach((element) => {
+  addEventListener("click", (event) => {
+    clickedBtn(event.target);
+  });
+});
 
-  fetch('../../events.json')
-    .then((response) => response.json())
-    .then((json) => {events = json.events; printEvents();});
+function clickedBtn(element) {
+  // if(element.innerHTML == "Всичко"){
+  //   console.log(element.innerHTML);
 
-    function printEvents() {
-      events.forEach(element => {
-        console.log(element.seasonTag);
-        if(element.seasonTag == "autumn" || element.seasonTag == "autumn"){
-          let currentCard = "".concat(card[0]);
-          currentCard = currentCard.concat(element.token);
-          currentCard = currentCard.concat(card[1]);
-  
-  
-          currentCard = currentCard.concat(element.imgURL);
-          currentCard = currentCard.concat(card[2]);
-          currentCard = currentCard.concat(element.eventName);
-  
-          currentCard = currentCard.concat(card[3]);
-          currentCard = currentCard.concat(card[4]);
-          currentCard = currentCard.concat(card[5]);
-          currentCard = currentCard.concat(card[6]);
-          currentCard = currentCard.concat(card[7]);
-          currentCard = currentCard.concat(card[8]);
-          currentCard = currentCard.concat(card[9]);
-          cardBox.innerHTML+=currentCard;
-        }
-
-
-      });
-
-    }
-
-
-  
-  // Add active class on another page linked
-  // ==========================================
-  // $(window).on('load',function () {
-  //     var current = location.pathname;
-  //     console.log(current);
-  //     $('#navbarSupportedContent ul li a').each(function(){
-  //         var $this = $(this);
-  //         // if the current path is like this link, make it active
-  //         if($this.attr('href').indexOf(current) !== -1){
-  //             $this.parent().addClass('active');
-  //             $this.parents('.menu-submenu').addClass('show-dropdown');
-  //             $this.parents('.menu-submenu').parent().addClass('active');
-  //         }else{
-  //             $this.parent().removeClass('active');
-  //         }
-  //     })
-  // });
-
-  
-  
+  // }
+}
